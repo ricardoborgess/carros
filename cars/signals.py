7c90503c4c4cb278e7d8dac324f679b95c84f7f0
Ricardo <ricardoborgespinna@gmail.com>
2025-01-30 18:48:20 -0300

    att

--- cars/signals.py
+++ cars/signals.py
@@ -2,7 +2,7 @@ from django.db.models.signals import pre_save, pre_delete, post_save, post_delet
 from django.db.models import Sum
 from django.dispatch import receiver
 from cars.models import Car, CarInventory
-from gemini_api.client import car_gemini_ai
+
 
 def car_inventory_update():
     cars_count = Car.objects.all().count() # Calcula a quantidade de carros que tem no estoque
@@ -14,15 +14,10 @@ def car_inventory_update():
         cars_value=cars_value
     )
 
-@receiver(pre_save, sender=Car)
-def car_pre_save(sender, instance, **kwargs):
-    if not instance.bio: 
-        ai_bio = car_gemini_ai( instance.model, instance.brand, instance.factory_year )
-        instance.bio = ai_bio
-
 @receiver(post_save, sender=Car)
 def car_post_save(sender, instance, **kwargs):
     car_inventory_update()
+
 
 
 @receiver(post_delete, sender=Car)
